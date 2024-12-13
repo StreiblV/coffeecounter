2413dd7561ce3940893378a155e03a84e0478fa4
StreiblV <viktoria.streibl@gmail.com>
2024-12-13 11:44:00 +0100

    Update Cache Reloads and Fav.ico

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
